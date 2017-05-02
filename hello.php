9d842a3f7a736da72ad0bc72f882acbffa3c6bf7
zheng <milcfee@gmail.com>
2017-05-02 15:16:10 +1000

    add hello test file

---
+++ hello.php
@@ -0,0 +1,30 @@
+
+
+<html>
+<head>
+    
+<style type="text/css">
+    
+.home-shortcuts {
+    margin: 0;
+    background: #EC5921;
+}
+
+
+</style>
+
+</head>
+
+<body>
+<?php
+
+echo "hello";
+
+
+?>
+    <div class="home-shortcuts">
+ <h2>What would you like to do today?</h2>
+    </div>
+
+</body>
+</html>
